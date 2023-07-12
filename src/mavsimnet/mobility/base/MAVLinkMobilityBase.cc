7c048ef9260dc7c1d00098e792dfb266a0182f3b
Josef Kamysek <josef@kamysek.com>
2023-07-12 15:52:54 -0300

    - Adjusted inet imports

--- src/mavsimnet/mobility/base/MAVLinkMobilityBase.cc
+++ src/mavsimnet/mobility/base/MAVLinkMobilityBase.cc
@@ -51,7 +51,7 @@ void MAVLinkMobilityBase::initialize(int stage)
         targetSystem = (par("targetSystem").intValue() == -1) ? lastId++ : par("targetSystem");
         targetComponent = par("targetComponent");
         vehicleType = static_cast<VehicleType>(par("vehicleType").intValue());
-        coordinateSystem = getModuleFromPar<IGeographicCoordinateSystem>(par("coordinateSystemModule"), this, true);
+        coordinateSystem = getModuleFromPar<IGeographicCoordinateSystem>(par("coordinateSystemModule"), this);
 
         rtScheduler = dynamic_cast<inet::RealTimeScheduler *>(getSimulation()->getScheduler());
 
